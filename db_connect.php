faab0c1209a45cb988974821553c898e79714ea4
Dipen Subedi <dipens@ictteach-www.its.utas.edu.au>
2025-05-25 04:40:36 +1000

    Updated db_connect and added user insertion function with hashed password

--- db_connect.php
+++ db_connect.php
@@ -1,20 +1,41 @@
 <?php
-// Start session only if it hasn't already been started
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+    // Start session only if it hasn't already been started
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    // Database connection function
+    function connect() {
+        $host = 'localhost';
+        $user = "kit202-group-26";
+        $password = "VwDePHbkyUxM";
+        $dbname = "kit202-group-26";
+
+        // Connect to the database
+        $conn = new mysqli($host, $user, $password, $dbname);
+        // Check for errors
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        return $conn;
+    }
+    
+    // Create a new user
+    function insertUser($username, $email, $password) {
+        $conn = connect();
+
+        //Hash the password
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        // Insert new user into the database
+        $stmt = $conn->prepare("INSERT INTO `User` (username, email, passwordHash, role) VALUES (?, ?, ?, 'member')");
+        if (!$stmt) {
+            die("Prepare failed: " . $conn->error);
+        }
+        $stmt->bind_param("sss", $username, $email, $hashedPassword);
+        if($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+    }
 }
-
-// Database creds
-$host = 'localhost';
-$user = "kit202-group-26";
-$password = "VwDePHbkyUxM";
-$dbname = "kit202-group-26";
-
-// Create connection
-$conn = new mysqli($host, $user, $password, $dbname);
-
-// Check for errors
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
