2236c1871c43ab892325142f5bae310647b6eded
Dipen Subedi <dipens@ictteach-www.its.utas.edu.au>
2025-05-28 02:10:41 +1000

    Added function to check if user is logged in

--- write.php
+++ write.php
@@ -6,6 +6,12 @@
         session_start();
     }
 
+    // Check if the user is logged in
+    if (!isset($_SESSION['username'])) {
+        //Redirect to login page if not logged in
+        header("Location: login.php");
+    }
+
     // Check the user's role
     $role = $_SESSION['role'] ?? 'visitor';
     if ($role === 'visitor') {
